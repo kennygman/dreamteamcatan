1ae92f19e86ce944bb43b12a5ec5e9333051df52
Jared Craig <jaredmcraig@gmail.com>
2015-02-27 19:16:47 -0700

    Implementing resourceBarController

--- student_01_03_15/java/src/client/resources/ResourceBarController.java
+++ student_01_03_15/java/src/client/resources/ResourceBarController.java
@@ -2,28 +2,36 @@ package client.resources;
 
 import java.util.*;
 
+import shared.definitions.ResourceType;
+import model.ModelFacade;
+import model.player.Player;
+import model.player.Resources;
 import client.base.*;
 
 
 /**
  * Implementation for the resource bar controller
  */
-public class ResourceBarController extends Controller implements IResourceBarController {
+public class ResourceBarController extends Controller implements IResourceBarController, Observer {
 
 	private Map<ResourceBarElement, IAction> elementActions;
 	
+	//--------------------------------------------------------------------------------
 	public ResourceBarController(IResourceBarView view) {
 
 		super(view);
 		
 		elementActions = new HashMap<ResourceBarElement, IAction>();
+		ModelFacade.getInstance().addObserver(this);
 	}
 
+	//--------------------------------------------------------------------------------
 	@Override
 	public IResourceBarView getView() {
 		return (IResourceBarView)super.getView();
 	}
 
+	//--------------------------------------------------------------------------------
 	/**
 	 * Sets the action to be executed when the specified resource bar element is clicked by the user
 	 * 
@@ -35,31 +43,37 @@ public class ResourceBarController extends Controller implements IResourceBarCon
 		elementActions.put(element, action);
 	}
 
+	//--------------------------------------------------------------------------------
 	@Override
 	public void buildRoad() {
 		executeElementAction(ResourceBarElement.ROAD);
 	}
 
+	//--------------------------------------------------------------------------------
 	@Override
 	public void buildSettlement() {
 		executeElementAction(ResourceBarElement.SETTLEMENT);
 	}
 
+	//--------------------------------------------------------------------------------
 	@Override
 	public void buildCity() {
 		executeElementAction(ResourceBarElement.CITY);
 	}
 
+	//--------------------------------------------------------------------------------
 	@Override
 	public void buyCard() {
 		executeElementAction(ResourceBarElement.BUY_CARD);
 	}
 
+	//--------------------------------------------------------------------------------
 	@Override
 	public void playCard() {
 		executeElementAction(ResourceBarElement.PLAY_CARD);
 	}
 	
+	//--------------------------------------------------------------------------------
 	private void executeElementAction(ResourceBarElement element) {
 		
 		if (elementActions.containsKey(element)) {
@@ -69,5 +83,46 @@ public class ResourceBarController extends Controller implements IResourceBarCon
 		}
 	}
 
+	//--------------------------------------------------------------------------------
+	@Override
+	public void update(Observable o, Object arg)
+	{
+		updateElements();
+	}
+
+	//--------------------------------------------------------------------------------
+	public void updateElements()
+	{
+		// WOOD, BRICK, SHEEP, WHEAT, ORE, ROAD, SETTLEMENT, CITY, BUY_CARD, PLAY_CARD, SOLDIERS
+
+		boolean valid = false;
+		Player player = ModelFacade.getInstance().getGame().getPlayers()
+				[ModelFacade.getInstance().getPlayerInfo().getPlayerIndex()];
+		Resources resources = player.getResources();
+		
+		setElement(ResourceBarElement.WOOD, resources.getResourceAmount(ResourceType.WOOD));
+		setElement(ResourceBarElement.SHEEP, resources.getResourceAmount(ResourceType.SHEEP));
+		setElement(ResourceBarElement.WHEAT, resources.getResourceAmount(ResourceType.WHEAT));
+		setElement(ResourceBarElement.ORE, resources.getResourceAmount(ResourceType.ORE));
+		setElement(ResourceBarElement.SOLDIERS, player.getSoldiers());
+
+		setElement(ResourceBarElement.ROAD, player.getRoads());
+		setElement(ResourceBarElement.SETTLEMENT, player.getSettlements());
+		setElement(ResourceBarElement.CITY, player.getCities());
+		
+		valid = ModelFacade.getInstance().CanBuyDevCard();
+		getView().setElementEnabled(ResourceBarElement.BUY_CARD, valid);
+
+//		updateElement(ResourceBarElement.PLAY_CARD, 0);
+		
+	}
+	
+	//--------------------------------------------------------------------------------
+	public void setElement(ResourceBarElement element, int amount)
+	{
+		getView().setElementAmount(element, amount);
+		getView().setElementEnabled(element, amount > 0);
+
+	}
 }
 

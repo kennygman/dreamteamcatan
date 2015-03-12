5ffb5af6bdbdbf5ea61598574c0012674172b4b8
camigreenall <cami.greenall@byu.net>
2015-03-12 17:49:32 -0600

    Updated Resources model and javadocs

--- student_01_03_15/java/src/model/player/Resources.java
+++ student_01_03_15/java/src/model/player/Resources.java
@@ -2,18 +2,23 @@ package model.player;
 
 import shared.definitions.ResourceType;
 
-
-public class Resources {
+public class Resources
+{
 
 	private int wood;
 	private int sheep;
 	private int wheat;
 	private int brick;
 	private int ore;
-	static ResourceType[] list = {ResourceType.WHEAT, ResourceType.WOOD,
-			ResourceType.BRICK, ResourceType.ORE, ResourceType.SHEEP};
-	
-	public Resources(int wood, int sheep, int wheat, int brick, int ore) 
+	static ResourceType[] list = { ResourceType.WHEAT, ResourceType.WOOD,
+			ResourceType.BRICK, ResourceType.ORE, ResourceType.SHEEP };
+
+	public Resources()
+	{
+		wood = wheat = sheep = brick = ore = 0;
+	}
+
+	public Resources(int wood, int sheep, int wheat, int brick, int ore)
 	{
 		this.wood = wood;
 		this.sheep = sheep;
@@ -26,6 +31,13 @@ public class Resources {
 	{
 		return list;
 	}
+
+	/**
+	 * Updates exisiting resources with new resources
+	 * 
+	 * @param r
+	 *            Resources to base the update off of
+	 */
 	public void update(Resources r)
 	{
 		this.wood = r.getResourceAmount(ResourceType.WOOD);
@@ -34,9 +46,8 @@ public class Resources {
 		this.brick = r.getResourceAmount(ResourceType.BRICK);
 		this.ore = r.getResourceAmount(ResourceType.ORE);
 	}
-	
+
 	/**
-	 * 
 	 * Used to facilitate trading.
 	 */
 	public Resources invert()
@@ -49,163 +60,227 @@ public class Resources {
 		return this;
 	}
 
-
 	/**
 	 * This method is called when a 7 is rolled by the Dice
+	 * 
 	 * @return the number of resources the player owns
 	 */
 	public int size()
 	{
-		return wood+sheep+wheat+brick+ore;
+		return wood + sheep + wheat + brick + ore;
 	}
-	
-	public Resources()
-	{
-		wood=wheat=sheep=brick=ore=0;
-	}
-	
+
 	/**
 	 * Adds resource to list
-	 * @param type they type of resource
-	 * @param n the number of resources
+	 * 
+	 * @param type
+	 *            they type of resource
+	 * @param n
+	 *            the number of resources
 	 */
 	public void addResource(ResourceType type, int n)
 	{
-		switch(type)
+		switch (type)
 		{
-		case WOOD: wood+=n; 
-		break;
-		case BRICK: brick+=n; 
-		break;
-		case WHEAT: wheat+=n; 
-		break;
-		case SHEEP: sheep+=n; 
-		break;
-		case ORE: ore+=n; 
-		break;
-			default:
-				break;
-		}
-	}
-	
-	/**
-	 * Removes resource from list
-	 * @param type the type of resource
-	 * @param n the number of resources
-	 */
-	public void useResource(ResourceType type, int n)
-	{
-		switch(type)
-		{
-		case WOOD: wood-=n; 
-		break;
-		case BRICK: brick-=n; 
-		break;
-		case WHEAT: wheat-=n; 
-		break;
-		case SHEEP: sheep-=n; 
-		break;
-		case ORE: ore-=n; 
-		break;
-			default:
-				break;
-		}
-	}
-	
-	public int getResourceAmount(ResourceType type)
-	{
-		int amount = 0;
-		switch(type)
-		{
-		case WOOD: amount = wood; 
-		break;
-		case BRICK: amount = brick;
-		break;
-		case WHEAT: amount = wheat;
-		break;
-		case SHEEP: amount = sheep;
-		break;
-		case ORE: amount = ore;
-		break;
-			default:
-				break;
-		}
-		return amount;
-		
-	}
-	
-	public void setResource(ResourceType type, int amount)
-	{
-		switch(type)
-		{
-		case WOOD: wood=amount; 
-		break;
-		case BRICK: brick=amount;
-		break;
-		case WHEAT: wheat=amount;
-		break;
-		case SHEEP: sheep=amount;
-		break;
-		case ORE: ore=amount;
-		break;
-			default:
-				break;
-		}
-	}
-	
-	public int getResourceAmount(String type)
-	{
-		int amount = 0;
-		switch(type)
-		{
-		case "wood": amount = wood; 
-		break;
-		case "brick": amount = brick;
-		break;
-		case "wheat": amount = wheat;
-		break;
-		case "sheep": amount = sheep;
-		break;
-		case "ore": amount = ore;
-		break;
-			default:
-				break;
-		}
-		return amount;
-		
-	}
-	
-	public void resetResource(ResourceType type)
-	{
-		switch(type)
-		{
-		case WOOD: wood = 0; 
-		break;
-		case BRICK: brick = 0;
-		break;
-		case WHEAT: wheat = 0;
-		break;
-		case SHEEP: sheep = 0;
-		break;
-		case ORE: ore = 0;
-		break;
-			default:
-				break;
+		case WOOD:
+			wood += n;
+			break;
+		case BRICK:
+			brick += n;
+			break;
+		case WHEAT:
+			wheat += n;
+			break;
+		case SHEEP:
+			sheep += n;
+			break;
+		case ORE:
+			ore += n;
+			break;
+		default:
+			break;
 		}
 	}
 
 	/**
-	 * This method verifies that the player has at least the same amount of resources in
-	 * the specified list
-	 * @param r the resources to compare with
-	 * @return true if each resource is greater than or equal to the amount in the comparison
+	 * Removes resource from list
+	 * 
+	 * @param type
+	 *            the type of resource
+	 * @param n
+	 *            the number of resources
+	 */
+	public void useResource(ResourceType type, int n)
+	{
+		switch (type)
+		{
+		case WOOD:
+			wood -= n;
+			break;
+		case BRICK:
+			brick -= n;
+			break;
+		case WHEAT:
+			wheat -= n;
+			break;
+		case SHEEP:
+			sheep -= n;
+			break;
+		case ORE:
+			ore -= n;
+			break;
+		default:
+			break;
+		}
+	}
+
+	/**
+	 * Returns the number of a specific resource in this particular resource
+	 * hand
+	 * 
+	 * @param type
+	 *            ResourceType to return amount of
+	 * @return number of that resource in this resources hand
+	 */
+	public int getResourceAmount(ResourceType type)
+	{
+		int amount = 0;
+		switch (type)
+		{
+		case WOOD:
+			amount = wood;
+			break;
+		case BRICK:
+			amount = brick;
+			break;
+		case WHEAT:
+			amount = wheat;
+			break;
+		case SHEEP:
+			amount = sheep;
+			break;
+		case ORE:
+			amount = ore;
+			break;
+		default:
+			break;
+		}
+		return amount;
+
+	}
+
+	/**
+	 * Changes how many of a specific resource there is
+	 * 
+	 * @param type
+	 *            ResourceType to set amount to
+	 * @param amount
+	 *            How much of this resource to set
+	 */
+	public void setResource(ResourceType type, int amount)
+	{
+		switch (type)
+		{
+		case WOOD:
+			wood = amount;
+			break;
+		case BRICK:
+			brick = amount;
+			break;
+		case WHEAT:
+			wheat = amount;
+			break;
+		case SHEEP:
+			sheep = amount;
+			break;
+		case ORE:
+			ore = amount;
+			break;
+		default:
+			break;
+		}
+	}
+
+	/**
+	 * Returns how many of a specific resource there is
+	 * 
+	 * @param type
+	 *            Which resource it is you are trying to get the amount of
+	 * @return amount of that resource there is
+	 */
+	public int getResourceAmount(String type)
+	{
+		int amount = 0;
+		switch (type)
+		{
+		case "wood":
+			amount = wood;
+			break;
+		case "brick":
+			amount = brick;
+			break;
+		case "wheat":
+			amount = wheat;
+			break;
+		case "sheep":
+			amount = sheep;
+			break;
+		case "ore":
+			amount = ore;
+			break;
+		default:
+			break;
+		}
+		return amount;
+
+	}
+
+	/**
+	 * Sets a specific resource to 0 amount
+	 * 
+	 * @param type
+	 *            resource to set to 0
+	 */
+	public void resetResource(ResourceType type)
+	{
+		switch (type)
+		{
+		case WOOD:
+			wood = 0;
+			break;
+		case BRICK:
+			brick = 0;
+			break;
+		case WHEAT:
+			wheat = 0;
+			break;
+		case SHEEP:
+			sheep = 0;
+			break;
+		case ORE:
+			ore = 0;
+			break;
+		default:
+			break;
+		}
+	}
+
+	/**
+	 * This method verifies that the player has at least the same amount of
+	 * resources in the specified list
+	 * 
+	 * @param r
+	 *            the resources to compare with
+	 * @return true if each resource is greater than or equal to the amount in
+	 *         the comparison
 	 */
 	public boolean contains(Resources r)
 	{
 		for (ResourceType res : list)
 		{
 			int amount = r.getResourceAmount(res);
-			if (amount > 0 && this.getResourceAmount(res) < amount) return false;
+			if (amount > 0 && this.getResourceAmount(res) < amount)
+				return false;
 		}
 		return true;
 	}
@@ -216,5 +291,5 @@ public class Resources {
 		return "Resources [wood=" + wood + ", sheep=" + sheep + ", wheat="
 				+ wheat + ", brick=" + brick + ", ore=" + ore + "]";
 	}
-	
+
 }

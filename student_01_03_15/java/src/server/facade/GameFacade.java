b54987a2b0aab400f651ee0aca547b373e0fe769
Jorge Gonzalez <gpg0nzal3z@gmail.com>
2015-03-23 15:05:21 -0600

    updated game facade

--- student_01_03_15/java/src/server/facade/GameFacade.java
+++ student_01_03_15/java/src/server/facade/GameFacade.java
@@ -1,17 +1,25 @@
 package server.facade;
 
+import java.util.List;
+
 import model.Game;
+import model.player.Player;
 import server.GameManager;
+import server.UserManager;
+import server.commands.ICommand;
 import shared.parameters.AddAiParam;
 import shared.response.*;
 
 public class GameFacade implements IGameFacade
 {
     private GameManager games;
+    private UserManager users;
+
     
-    public GameFacade(GameManager games)
+    public GameFacade(GameManager games, UserManager users)
     {
     	this.games=games;
+    	this.users= users;
     }
     
 	@Override
@@ -29,24 +37,59 @@ public class GameFacade implements IGameFacade
 	@Override
 	public CommandResponse getCommands(int id)
 	{
-		games.getCommands(id);
-		return null;
+			
+		String commands = convertToString(games.getCommands(id));
+		
+		CommandResponse response = new CommandResponse(commands,true);
+		return response;
 	}
 
 	@Override
 	public StandardResponse commands(int id)
 	{
-		// exectues list of commands
 		
-		return null;
+		List<ICommand> commands = games.getCommands(id);
+	
+		//execute list of commands
+		for(int i=0; i<commands.size();i++)
+		{
+			commands.get(i).execute();
+		}
+		
+		StandardResponse response = new StandardResponse(true);
+		
+		return response;
 	}
 
 	@Override
 	public StandardResponse addAI(AddAiParam param, int id)
 	{
+		StandardResponse response = new StandardResponse(false);
+		boolean insertedAI = false;
+		if(param.getType()=="LARGEST_ARMY")
+		{
+			response = new StandardResponse(true);
+			Game currentGame = games.getGame(id);
+			Player[] players = currentGame.getPlayers();
+			
+			for(int i=0;i<4;i++)
+			{
+				if(players[i] == null)
+				{
+					
+					insertedAI=true;
+					
+					
+				}
+			}
+			
+			
+			
+			
+			
+		}
 		
-		StandardResponse response = new StandardResponse(true);
-		
+	
 		return response;
 		
 	}
@@ -73,5 +116,17 @@ public class GameFacade implements IGameFacade
     	}
     	
     	return response;
+	}
+	private String convertToString(List<ICommand> commands)
+	{
+		StringBuilder result = new StringBuilder();
+		
+		for(int i=0; i<commands.size();i++)
+		{
+			result.append(commands.get(i));
+			result.append("\n");
+		}
+		
+		return result.toString();
 	}
 }
